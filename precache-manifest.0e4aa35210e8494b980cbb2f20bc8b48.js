f396b2c0b56f7782139dfc77b666a084c7bc39c1
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-07-23 15:51:43 +0300

    Updates

--- precache-manifest.0e4aa35210e8494b980cbb2f20bc8b48.js
+++ precache-manifest.0e4aa35210e8494b980cbb2f20bc8b48.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    revision: 'bed13d015cfde80719a7a15836dcb27c',
-    url: '/TaskTimerReact/index.html',
+    "revision": "bed13d015cfde80719a7a15836dcb27c",
+    "url": "/TaskTimerReact/index.html"
   },
   {
-    revision: '215e71eea7e9723a23a5',
-    url: '/TaskTimerReact/static/css/main.4c635d3b.chunk.css',
+    "revision": "215e71eea7e9723a23a5",
+    "url": "/TaskTimerReact/static/css/main.4c635d3b.chunk.css"
   },
   {
-    revision: 'cc5e579791ce34f36eea',
-    url: '/TaskTimerReact/static/js/2.24fb0a4a.chunk.js',
+    "revision": "cc5e579791ce34f36eea",
+    "url": "/TaskTimerReact/static/js/2.24fb0a4a.chunk.js"
   },
   {
-    revision: '215e71eea7e9723a23a5',
-    url: '/TaskTimerReact/static/js/main.2569f486.chunk.js',
+    "revision": "215e71eea7e9723a23a5",
+    "url": "/TaskTimerReact/static/js/main.2569f486.chunk.js"
   },
   {
-    revision: '1235945606704f6381b1',
-    url: '/TaskTimerReact/static/js/runtime~main.02e0ce82.js',
-  },
+    "revision": "1235945606704f6381b1",
+    "url": "/TaskTimerReact/static/js/runtime~main.02e0ce82.js"
+  }
 ]);
